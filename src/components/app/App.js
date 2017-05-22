83ee0fd56aa8c3ce7420297ae4bd7e945f8a1c9a
Sergei Sviridov <sergei.sviridov@readify.net>
2017-05-22 12:48:37 +1000

    Moved provide to app component level so that tests do not fail

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -8,16 +8,24 @@ import GitHubConverter from '../converters/GitHubConverter';
 import GitLabConverter from '../converters/GitLabConverter';
 import MarkdownConverter from '../converters/MarkdownConverter';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
+import app from '../../reducers'
+
+const store = createStore(app);
+
 const App = (props) => {
     return (
-        <Router>
-          <div>
-            <Route exact path="/" component={GitHubConverter} />
-            <Route path="/github" component={GitHubConverter} />
-            <Route path="/gitlab" component={GitLabConverter} />
-            <Route path="/md" component={MarkdownConverter} />
-          </div>
-        </Router>
+        <Provider store={store}>
+          <Router>
+            <div>
+              <Route exact path="/" component={GitHubConverter} />
+              <Route path="/github" component={GitHubConverter} />
+              <Route path="/gitlab" component={GitLabConverter} />
+              <Route path="/md" component={MarkdownConverter} />
+            </div>
+          </Router>
+        </Provider>
     );
 };
 
